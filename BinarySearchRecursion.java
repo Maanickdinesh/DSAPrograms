4e08216f2eac12c78e841daf6e6a16627b32984e
Dineshkumar M <125424043+Maanickdinesh@users.noreply.github.com>
2024-02-20 11:02:06 +0530

    BinarySearchRecursion.java

--- BinarySearchRecursion.java
+++ BinarySearchRecursion.java
@@ -3,7 +3,40 @@ public class BinarySearchRecursion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int arr[] = { 3, 6, 7, 8, 9, 10, 14 };
+		int target = 14;
+        int result = binarySearch(arr,target,0,arr.length);
+        if(result !=-1) {
+			System.out.println("Element found in "+ result );
+		}
+		else {
+			System.out.println("Element not  found" );
+		}
+	}
 
+	public static int binarySearch(int[] arr, int target,int left,int right) {
+		// TODO Auto-generated method stub
+		
+			if(left<= right) {
+				int mid = (left+right)/2;
+				if(arr[mid]==target) {
+					return mid;
+				}
+				
+				else if(arr[mid]<target) {
+					return binarySearch(arr,target,mid+1,right);
+				}
+				else {
+					return binarySearch(arr,target,left,mid-1);
+				}
+			}
+			
+			
+		
+		
+		
+		
+		return -1;
 	}
 
 }
